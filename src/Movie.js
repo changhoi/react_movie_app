af81449e2f9160e1bf97740d1f3c98a323c7d142
changhoi <0417kch@naver.com>
2019-03-14 14:09:55 +0900

    props 마침

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./Movie.css";
 
 class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    poster: PropTypes.string
+  };
   render() {
     console.log(this.props);
     return (
       <div>
-        <MoviePoster />
+        <MoviePoster poster={this.props.poster} />
         <h1>{this.props.title}</h1>;
       </div>
     );
@@ -14,10 +19,11 @@ class Movie extends Component {
 }
 
 class MoviePoster extends Component {
+  static propTypes = {
+    poster: PropTypes.string.isRequired
+  };
   render() {
-    return (
-      <img src="https://scontent.ficn6-1.fna.fbcdn.net/v/t1.0-9/20430132_1352389891525050_3591124631438117403_n.jpg?_nc_cat=103&_nc_ht=scontent.ficn6-1.fna&oh=103b4026e2f983dba98a2f66d425a506&oe=5D2105F3" />
-    );
+    return <img src={this.props.poster} />;
   }
 }
 
