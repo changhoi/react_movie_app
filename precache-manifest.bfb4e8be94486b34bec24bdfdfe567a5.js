95c240d5e8e29b205999b1c6189f73b725d90091
changhoi <0417kch@naver.com>
2019-03-17 22:02:09 +0900

    Updates

---
+++ precache-manifest.bfb4e8be94486b34bec24bdfdfe567a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb96f4bf6b9807c2693",
+    "url": "/movie_app/static/css/main.2776c681.chunk.css"
+  },
+  {
+    "revision": "6fb96f4bf6b9807c2693",
+    "url": "/movie_app/static/js/main.4b30e5dc.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "75ca2b9e1ca28f58fd3c",
+    "url": "/movie_app/static/js/2.762322e5.chunk.js"
+  },
+  {
+    "revision": "cd8ac84d395d91c9eede1d77eb2e767a",
+    "url": "/movie_app/index.html"
+  }
+];
