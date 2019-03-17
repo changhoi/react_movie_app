ff070ee99879cf8ad3e844a7c91ee92d0d9811b3
changhoi <0417kch@naver.com>
2019-03-17 21:05:41 +0900

    fetch 해서 토렌트 영화 정보를 받아왔음, prop으로 넘기고, state에 저장

--- src/App.js
+++ src/App.js
@@ -5,39 +5,42 @@ import Movie from "./Movie";
 class App extends Component {
   state = {};
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "metrix",
-            poster:
-              "https://movie-phinf.pstatic.net/20111224_282/1324652930159TOMLl_JPEG/movie_image.jpg?type=m665_443_2"
-          },
-          {
-            title: "Old boy",
-            poster:
-              "https://movie-phinf.pstatic.net/20111222_145/1324537095951YRCQl_JPEG/movie_image.jpg?type=m665_443_2"
-          },
-          {
-            title: "Zelda",
-            poster:
-              "http://blogfiles.naver.net/MjAxNzExMDRfMjQ1/MDAxNTA5ODA0NTE2ODEy.cr_VQlnDxPYO0gxKKpcpVqosdA90JahwGIytjYCY4Acg.VWLBCH9Vv5evxxsjaA_JQ9BEYBuunlVt_qLF_4bCUEgg.JPEG.ehdrbfb/breath-main.jpg"
-          }
-        ]
-      });
-    }, 3000);
+    this._getMovies();
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => {
+        //console.log(json.data.movies);
+        return json.data.movies;
+      })
+      .catch(err => console.log(err));
+  };
+
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => (
-      <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => (
+      <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        key={movie.id}
+      />
     ));
+    console.log(movies);
 
     return movies;
   };
 
   render() {
-    console.log("render");
     return (
       <div className="App">
         {this.state.movies ? this._renderMovies() : "Loading"}
